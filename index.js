060dcbdff50d54741698653a2b5069e1d30a3d43
Mary Williams <91165055+mkriscodes@users.noreply.github.com>
2022-11-01 12:50:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+const passwordOne = document.getElementById('password-one')
+const passwordTwo = document.getElementById('password-two')
+let isGenerated = false
+
+function getRandomChar () {
+    for(let i = 0; i < characters.length; i++) {
+        let char = Math.floor(Math.random() * characters.length)
+        return characters[char]
+    }
+}
+
+function generatePassword () {
+    if(isGenerated === false) {
+        let passwordLength = 15
+        for(let i = 0; i < passwordLength; i++) {
+            passwordOne.textContent += getRandomChar ()
+            passwordTwo.textContent += getRandomChar ()
+            isGenerated = true
+        }
+    }
+}
